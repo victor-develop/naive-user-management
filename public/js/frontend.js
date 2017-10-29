345e51a48f65988177f2775ceb4fbbde3e5b9ad5
victor-develop <victor.develop@outlook.com>
2017-10-29 16:37:03 +0100

    add: setup angularjs

---
+++ public/js/frontend.js
@@ -0,0 +1,5 @@
+var frontend = angular.module('frontend', ['angular-loading-bar'])
+    .controller('MainCtrl', [function() {
+        var self = this;
+        
+    }]);
--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -1,11 +1,22 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html ng-app="frontend" ng-controller="MainCtrl as Main" lang="{{ app()->getLocale() }}">
 <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Naive User Management</title>
+
+        <!-- Angular -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.9/angular.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.9/angular-sanitize.min.js"></script>
+
+        <!-- Angular HTTP Loading bar -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/angular-loading-bar/0.9.0/loading-bar.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-loading-bar/0.9.0/loading-bar.min.js"></script>
+
+        <!-- Application Logic -->
+        <script src="/js/frontend.js"></script>
 
         <!-- Styles -->
         <style>
