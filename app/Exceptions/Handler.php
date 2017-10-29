25c5f96a2acd31c3860f928f455c3bcbc96c6d2a
victor-develop <victor.develop@outlook.com>
2017-10-29 16:17:48 +0100

    reuse: unified exception handling logic, using response macro

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -39,15 +39,76 @@ class Handler extends ExceptionHandler
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        return parent::render($request, $exception);
-    }
+        /*
+            Exception Response format:
+                {
+                    "success": false,
+                    "data": {
+                        "error_key": "validation",
+                        "debug": {
+                        "exception": "",
+                        "message": "The given data was invalid.",
+                        "trace": []
+                        },
+                        "errors": {
+                        "name": [
+                            "The name field is required."
+                        ]
+                        }
+                    },
+                    "error_messages": [
+                        "Something went wrong."
+                    ]
+                }      
+         */
+	    if ($request->wantsJson())
+        {
+
+                $messages = [
+                    "Something went wrong."
+                ];
+
+                $data = [
+                    'error_key' => 'unknown'
+                ];
+
+                // If the app is in debug mode
+                if (config('app.debug'))
+                {
+                    $debugBag = [];
+                    // Add the exception class name, message and stack trace to response
+                    $debugBag['exception'] = get_class($e); // Reflection might be better here
+                    $debugBag['message'] = $e->getMessage();
+                    $debugBag['trace'] = $e->getTrace();
+                    $data['debug'] = $debugBag;
+                }
+
+                // Default response of 400
+                $status = 400;
+
+
+                if (($e instanceof ValidationException)) {
+                    $data["error_key"] = 'validation';
+                    $data["errors"] = $e->errors();      
+                    $status = $e->status;             
+                }
+
+                // If this exception is an instance of HttpException
+                if ($this->isHttpException($e))
+                {
+                    // Grab the HTTP status code from the Exception
+                    $status = $e->getStatusCode();
+                    $messages[] = $e->getMessage();
+                }
+
+                // Return a JSON response with the response array and status code
+                return response()->errors($messages, $status, $data);
+          }
+
+          // Default to the parent class' implementation of handler
+          
+          return parent::render($request, $e);
+      }
 }
