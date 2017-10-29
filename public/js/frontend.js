f73dce44b64f522e28a3443da1d72e82660b7f24
victor-develop <victor.develop@outlook.com>
2017-10-29 16:39:13 +0100

    add: NoticeService

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,5 +1,12 @@
 var frontend = angular.module('frontend', ['angular-loading-bar'])
-    .controller('MainCtrl', [function() {
+    .controller('MainCtrl', function(NoticeService) {
         var self = this;
-        
-    }]);
+        NoticeService.alert('NoticeService is ready');
+    })
+    .factory('NoticeService', function(){
+        var service = {};
+        service.alert = function(info) {
+            alert(info);
+        };
+        return service;
+    })
