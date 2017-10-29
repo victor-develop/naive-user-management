ba1fa706f98b2eaecbcb5c45171656fe12d761ad
victor-develop <victor.develop@outlook.com>
2017-10-29 20:45:52 +0100

    add: frontend: color indication on nav item

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -83,81 +83,9 @@ var frontend = angular.module('frontend', ['angular-loading-bar','ui.router'])
         }
         
     })    
-    .controller('MainCtrl', function(NoticeService, $http) {
+    .controller('MainCtrl', function(NoticeService, $state) {
         var self = this;
-
-        // TODO: test scripts, should be removed
-    /*
-        var u = {name: ''};
-        u.name = makeid();
-        var emptyu = {};
-        $http.post('/appusers/create', u);
-        $http.post('/appusers/create', emptyu);
-
-        var g = {name: ''};
-        g.name = "group_" + makeid();
-        var emptygroup = {};
-        $http.post('/groups/create', g);
-        $http.post('/groups/create', emptygroup);   
-        
-
-        
-        $http.get('groups')
-        .then((response) => {
-            return response.data.data.map(g => g.id);
-        })
-        .then((group_ids)=> {
-            var user_withGroup = {name: '', groups: []};
-            user_withGroup.name = "ug" + makeid();
-            user_withGroup.groups = getRandom(group_ids, 3);
-            // test create
-            $http.post('/appusers/create', user_withGroup); 
-
-            // test save
-            $http.get('appusers?expand=all')
-            .then( (xhrres) =>{
-               return xhrres.data.data;
-            })
-            .then((users)=> {
-                return getRandom(users,1)[0];
-            })
-            .then((firstUser)=>{
-                var editedUser = {name: firstUser.name, id: firstUser.id, groups:[]};
-                editedUser.groups = getRandom(group_ids, 3);
-                $http.post('/appusers/save', editedUser);
-            })
-        });
-
-        function makeid() {
-            var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-          
-            for (var i = 0; i < 12; i++)
-              text += possible.charAt(Math.floor(Math.random() * possible.length));
-          
-            return text;
-        }
-
-        function getRandom(arrayNum, count) {
-            // Make a copy of the array
-            var tmp = arrayNum.slice(arrayNum);
-            var ret = [];
-            
-            for (var i = 0; i < count; i++) {
-              var index = Math.floor(Math.random() * tmp.length);
-              var removed = tmp.splice(index, 1);
-              // Since we are only removing one element
-              ret.push(removed[0]);
-            }
-            return ret;  
-          }        
-
-        function assert(predicate) {
-            if (!predicate) {
-                alert("wrong!");
-            }
-        }
-    */
+        self.$state = $state;
     })
     .factory('NoticeService', function() {
         var service = {};
