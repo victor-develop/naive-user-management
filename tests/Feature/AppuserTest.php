3b7c48b7a94cb9cf05f926ebad298aa3ddac7a3e
victor-develop <victor.develop@outlook.com>
2017-11-10 08:39:11 +0100

    add: test: /appusers/create

--- tests/Feature/AppuserTest.php
+++ tests/Feature/AppuserTest.php
@@ -14,8 +14,19 @@ class AppuserTest extends TestCase
      */
     public function testRoot()
     {
-        $response = $this->json('GET', 'appusers');
+        $response = $this->json('GET', '/appusers');
 
         $response->assertStatus(200);
+    }
+
+    public function testCreate()
+    {
+        $response = $this->json('POST', '/appusers/create');
+        $response->assertStatus(422);
+        $response->assertJson([
+            'data' => [
+                'error_key' => 'validation'
+            ]
+        ]);
     }
 }
